1810b50a344fec3ee3991cc771060dc3121cb7d6
ZisisKostakakisGithubPersonal <dev@zisiskostakakis.com>
2024-12-10 13:41:40 +0000

    Adding zsh plugins

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -54,3 +54,10 @@ brew install --cask rectangle
 for f in "${dependencies[@]}"; do
   brew install "$f"
 done
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" "$ZSH_CUSTOM/plugins/autoswitch_virtualenv"
+git clone "https://github.com/Aloxaf/fzf-tab" "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab"
+git clone "https://github.com/MichaelAquilina/zsh-you-should-use.git" "$ZSH_CUSTOM/plugins/you-should-use"
+git clone "https://github.com/zsh-users/zsh-autosuggestions" "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
